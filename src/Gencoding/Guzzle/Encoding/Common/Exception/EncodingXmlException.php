1488e6a05365128657df983d36e748d01268ce80
Svilborg <svilborg@gmail.com>
2013-08-11 21:39:14 +0300

    Refactoring

--- src/Gencoding/Guzzle/Encoding/Common/Exception/EncodingXmlException.php
+++ src/Gencoding/Guzzle/Encoding/Common/Exception/EncodingXmlException.php
@@ -11,13 +11,6 @@ class EncodingXmlException extends \Exception implements GuzzleException
 {
 
     /**
-     * XML content
-     *
-     * @var EncodingResponse
-     */
-    protected $response;
-
-    /**
      * Exception message
      *
      * @var string
@@ -32,22 +25,10 @@ class EncodingXmlException extends \Exception implements GuzzleException
      */
     public function __construct($xmlResponse)
     {
-        $this->response = $xmlResponse;
-
         if ($xmlResponse->hasError()) {
             $this->message = $xmlResponse->getErrorMessage();
         }
 
         parent::__construct($this->message);
-    }
-
-    /**
-     * Returns the response for the XML exception.
-     *
-     * @return EncodingResponse The response object
-     */
-    public function getResponse()
-    {
-        return $this->response;
     }
 }
